f3d1db0a351088bc7b21b699a9bb018410b7365a
Tobias <gotteron89tb@gmail.com>
2021-12-16 15:40:25 +0100

    start with clean-code and awnser questions

---
+++ src/main/java/ch/bbw/zork/Zork_Run.java
@@ -0,0 +1,14 @@
+package ch.bbw.zork;
+
+/**
+ * Author: Tobias Bertschi, Yves Huber
+ */
+
+public class Zork_Run {
+
+    public static void main(String[] args) {
+        Game zorkgame = new Game();
+        zorkgame.play();
+    }
+
+}
