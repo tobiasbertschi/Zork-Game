d39a60b5ebb9ef349ded03f1589eaf98c6edcb32
Tobias <gotteron89tb@gmail.com>
2022-01-13 09:49:42 +0100

    add command items and create method printItems

--- src/main/java/ch/bbw/zork/Item.java
+++ src/main/java/ch/bbw/zork/Item.java
@@ -20,4 +20,28 @@ public class Item {
         this.name = name;
     }
 
+    public Types getType() {
+        return type;
+    }
+
+    public void setType(Types type) {
+        this.type = type;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
